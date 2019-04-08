4c84c002020aac64034168f55fdca6c847a8ee73
James E. Marca <james@activimetrics.com>
2019-04-07 23:33:29 -0700

    hacking around auto rebuild setup

--- context/entrypoint.sh
+++ context/entrypoint.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 set -e
 
-# source /opt/context/tex/setuptex
-inotifywait -m -e modify . | awk '$3 ~ /\.tex/ {system("context ")}'
+if [ -z "$TEXFILE" ]; then
+	echo >&2 'error: missing TEXFILE environment variable'
+	echo >&2 '  try running again with -e TEXFILE=<your context master file>'
+	exit 1
+fi
+echo "TEXFILE is $TEXFILE"
+PROCESS="/work/${TEXFILE}"
+ls ${PROCESS}
 
-"context /home/user/$@"
+# context /work/$TEXFILE
+while [ 1 ]; do
+                inotifywait --exclude *~ -e modify . | awk -v report=$PROCESS '$3 ~/\.(tex|md.?)$/ {system("context " report )}'
+                echo "----------"
+done
+
+
+# inotifywait  --exclude *~ -e modify /work | awk '$3 ~/\.(tex|md.?)$/ {system("ls " $3)}'
+#/work/$TEXFILE)}'
+
+#"context /work/$@"
