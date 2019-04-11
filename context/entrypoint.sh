f4c5936c0d36c1b38842d3c484c22b8b6654f10a
James E. Marca <james@activimetrics.com>
2019-04-11 10:55:56 -0700

    loop and listen for changes to md, tex files

--- context/entrypoint.sh
+++ context/entrypoint.sh
@@ -13,11 +13,14 @@ ls ${PROCESS}
 # context /work/$TEXFILE
 while [ 1 ]; do
                 inotifywait --exclude *~ -e modify . | awk -v report=$PROCESS '$3 ~/\.(tex|md.?)$/ {system("context " report )}'
-                echo "----------"
+                echo "---------- context done, looping ----------"
 done
 
+# not sure why the following doesn't work.  It works from slackware,
+# but not from within docker container.  I dunno
 
-# inotifywait  --exclude *~ -e modify /work | awk '$3 ~/\.(tex|md.?)$/ {system("ls " $3)}'
-#/work/$TEXFILE)}'
+# inotifywait -m --exclude *~ -e modify . | awk -v report=$PROCESS '$3 ~/\.(tex|md.?)$/ {system("context " report )}'
+
+
 
 #"context /work/$@"
